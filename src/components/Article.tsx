68a14f9aa8c7fd2b0e5b0fee0e60231820af37b3
usernamekun <2714446242@qq.com>
2022-06-16 22:08:41 +0800

    完成了文章数据的渲染操作

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,28 +1,34 @@
 import { connect, useDispatch, useSelector } from 'react-redux';
 import React from 'react'
+import back from '@/assets/back.jpg'
 import { useEffect } from 'react'
-import avatar from '../assets/back.jpg'
 import { getArticle } from '../store/actions/Article';
 import { RootState } from '../types/store';
 function Article() {
     const dispatch = useDispatch()
     const {active} = useSelector((state:RootState)=>state.channelReducer)
+    const {results} = useSelector((state: RootState)=> state.articleReducer)
     useEffect(() => {
         dispatch(getArticle(active,Date.now()))
     },[dispatch,active])
     return (
         <div className='list'>
-            <div className='article_item'>
-                <h3 className='van-ellipsis'>python数据预处理 ：数据标准化</h3>
-                <div className='img_box'>
-                    <img src={avatar} className='w100' alt='' />
+            {
+                results.map(item=>(
+                    <div key={item.art_id} className='article_item'>
+                    <h3 className='van-ellipsis'>{item.title}</h3>
+                    <div className='img_box'>
+                        <img src={item.cover.images?item.cover.images[0]:back} className='w100' />
+                    </div>
+                    <div className='info_box'>
+                        <span>13552285417</span>
+                        <span>{item.comm_count}评论</span>
+                        <span>{item.pubdate}</span>
+                    </div>
                 </div>
-                <div className='info_box'>
-                    <span>13552285417</span>
-                    <span>0评论</span>
-                    <span>2018-11-29T17:02:09</span>
-                </div>
-            </div>
+                ))
+            } 
+           
         </div>
     )
 }
