0662989e064ac969842e787df7916eba3ac4f8e7
usernamekun <2714446242@qq.com>
2022-06-17 09:41:25 +0800

    完结撒花

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,5 +10,5 @@ import { ChannelItem, ArticleItem } from '../types/data';
 //   url: 'channels', method: 'get'
 // })
 // 或者
-export const getChannelsList = () => requests.get<{channels: ChannelItem[] }>('channels')
+export const getChannelsList = () => requests.get<{channels: ChannelItem[] }>('channels');
 export const getArticleList = (id: number, time: number) => requests.get<{ results: ArticleItem[] }>(`articles?channel_id=${id}&timestamp=${time}`)
